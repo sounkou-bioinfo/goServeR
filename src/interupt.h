e5dab6b05cbb170c201a36e91eee3177f2049ce0
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-10-02 01:36:35 +0400

    remove extern

--- src/interupt.h
+++ src/interupt.h
@@ -3,12 +3,13 @@
 #include <R.h>
 #include <Rinternals.h>
 #include <R_ext/Visibility.h>
+#include <R_ext/Utils.h>
 // ref : https://github.com/cran/curl/blob/master/src/interrupt.c
 // https://stackoverflow.com/questions/40563522/r-how-to-write-interruptible-c-function-and-recover-partial-results
 
 // These functions are defined in R_ext/Utils.h
-extern void R_CheckUserInterrupt(void);
-extern Rboolean R_ToplevelExec(void (*fun)(void *), void *data);
+void R_CheckUserInterrupt(void);
+Rboolean R_ToplevelExec(void (*fun)(void *), void *data);
 
 // Only declare the function, implementation is in interupt.c
 int pending_interrupt(void);
