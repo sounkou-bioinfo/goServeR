ef3ca85539515c7bda8e33350f736319528922ce
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-10-07 01:20:02 +0400

    ci tls issue

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,5 @@
 library(goserveR)
 if (requireNamespace("tinytest", quietly = TRUE)) {
-    # Sys.setenv("CI" = "CI")
+    Sys.setenv("CI" = "CI")
     tinytest::test_package("goserveR", at_home = FALSE)
 }
