c4213e6d9b8d74abff9064adcaedf3d4e91d31c7
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-10-08 03:43:56 +0400

    push

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,6 @@
 library(goserveR)
 if (requireNamespace("tinytest", quietly = TRUE)) {
   Sys.setenv("CI" = "CI")
-  tinytest::test_package("goserveR", at_home = FALSE, ncpu = 1)
+  library(tinytest)
+  test_package("goserveR", at_home = FALSE, ncpu = NULL)
 }
