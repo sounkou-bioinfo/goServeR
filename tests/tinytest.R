8ec3c2788c4b7d360305754585a4025bf2661c7c
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-10-26 00:36:53 +0400

    run the tests

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,6 @@
 library(goserveR)
 if (requireNamespace("tinytest", quietly = TRUE)) {
-  Sys.setenv("CI" = "CI")
+  # Sys.setenv("CI" = "CI")
   library(tinytest)
   test_package("goserveR", at_home = FALSE, ncpu = NULL)
 }
